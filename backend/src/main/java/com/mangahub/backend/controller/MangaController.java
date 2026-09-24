01c0a9395eb0e7fe0971405c4fcf13799e348956
Thang <vietthang05p@gmail.com>
2026-09-24 22:05:54 +0700

    Refactor MangaController imports for clarity and organization

--- backend/src/main/java/com/mangahub/backend/controller/MangaController.java
+++ backend/src/main/java/com/mangahub/backend/controller/MangaController.java
@@ -4,14 +4,15 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mangahub.backend.dto.request.MangaCreateRequest;
 import com.mangahub.backend.dto.response.ApiResponse;
 import com.mangahub.backend.dto.response.MangaResponse;
 import com.mangahub.backend.service.MangaService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
